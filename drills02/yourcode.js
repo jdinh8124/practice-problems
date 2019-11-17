340f80948efa17bfa0e3f1399f549b3d35d19474
James Dinh <mrjames8124@gmail.com>
2019-11-17 15:58:29 -0800

    finished drills 1/2 and practice problem 25

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,17 +1,68 @@
+function countOccurences(arrayParam, wordNeedle){
+  var counter = 0;
+  for(var i =0; i < arrayParam.length; i++){
+    if(arrayParam[i] === wordNeedle){
+      counter += 1;
+    }
+  }
+  return counter;
+}
 
-
-function countOccurences(){
+function wordLengths(wordsArray){
+  var newArray = [];
+  for(var i =0; i < wordsArray.length; i++){
+    numToPush = 0;
+    numToPush = wordsArray[i].length;
+    newArray.push(numToPush);
+  }
+  return newArray;
 
 }
 
-function wordLengths(){
-
+function getMinMaxMean(arrayToMath){
+  var mathObject = {};
+  var greatestNum = null;
+  var smallestNum = null;
+  var mean = null;
+  for(var i =0; i < arrayToMath.length; i++){
+    mean += arrayToMath[i];
+    if (greatestNum < arrayToMath[i]){
+      greatestNum = arrayToMath[i];
+    }
+    if(smallestNum === null ){
+      smallestNum = arrayToMath[i];
+    }
+    else if (smallestNum > arrayToMath[i]) {
+      smallestNum = arrayToMath[i];
+    }
+  }
+  var meanAnswer = mean / arrayToMath.length;
+  mathObject.min = smallestNum;
+  mathObject.max = greatestNum;
+  mathObject.mean = meanAnswer;
+  return mathObject;
 }
 
-function getMinMaxMean(){
+function findMode(arrayNum){
+  var times = 0;
+  var highestOcc = null;
+  var mostNum = null;
+  for(var i =0; i <arrayNum.length; i++){
 
-}
+    for(var m = 0; m <arrayNum.length; m++){
+      if(arrayNum[i] === arrayNum[m]){
+        times+=1;
+        if (highestOcc <= times) {
+          highestOcc = times;
+          mostNum = arrayNum[i];
+        }
+      }
 
-function findMode(){
-
+    }
+    times = 0;
+  }
+//   - findMode: Given an array of numbers, find the number that occurs the most often for example:
+//     - input[5, 2, 7, 18, 2, 42, 5, 2]
+//       - returns 2, since it occurs 3 times.If 2 numbers occur the same amount, return the last one
+  return mostNum;
 }
