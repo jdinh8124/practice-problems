d93c6a34e4a100b64712c72433bc612ba143a986
James Dinh <mrjames8124@gmail.com>
2019-11-17 23:03:59 -0800

    working through drill 3

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -2,8 +2,22 @@
 // 	- example: 'folder1/folder2/folder3'
 //     - returns['folder1', 'folder2', 'folder3']
 
-function getPath(){
+// var findingTheStartOfSource = foundImg.lastIndexOf('/') + 1;
+// var findingSpotOfImgSource = foundImg.lastIndexOf('.');
+// var imageUrlWithoutPath = foundImg.slice(findingTheStartOfSource, findingSpotOfImgSource);
 
+
+
+function getPath(path){
+  var sliced = [];
+  var startOfLastDash = path.lastIndexOf("/");
+  var startofFirst = path.indexOf("/")
+  var firstFolder = path.slice(0, startofFirst);
+  var secondFolder = path.slice(startofFirst+1, startOfLastDash);
+  var thirdFolder = path.slice(startOfLastDash+1, path.length);
+  sliced.push(firstFolder, secondFolder, thirdFolder);
+
+  return sliced;
 }
 
 function getPathParts(){
