46dcf5a97caef39f57757f374515d26c2360e190
James Dinh <mrjames8124@gmail.com>
2019-11-17 22:34:11 -0800

    making a new branch

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -1,4 +1,6 @@
-
+// - getPath: given a path string, return an array with the different parts of the path for
+// 	- example: 'folder1/folder2/folder3'
+//     - returns['folder1', 'folder2', 'folder3']
 
 function getPath(){
 
