ad10e0bedc710b534adfb730b55b7128bd0f4991
James Dinh <mrjames8124@gmail.com>
2019-11-21 22:40:55 -0800

    still working through problems

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -5,23 +5,32 @@
 // var findingTheStartOfSource = foundImg.lastIndexOf('/') + 1;
 // var findingSpotOfImgSource = foundImg.lastIndexOf('.');
 // var imageUrlWithoutPath = foundImg.slice(findingTheStartOfSource, findingSpotOfImgSource);
+// var startOfLastDash = path.lastIndexOf("/");
+// var startofFirst = path.indexOf("/")
+// var firstFolder = path.slice(0, startofFirst);
+// var secondFolder = path.slice(startofFirst + 1, startOfLastDash);
+// var thirdFolder = path.slice(startOfLastDash + 1, path.length);
+// sliced.push(firstFolder, secondFolder, thirdFolder);
 
-
+// - getPathParts: Given a full URL string, break it up into parts in an object For
+//   - example: 'http://localhost:8000/happy/go/lucky/file.html'
+//     - return { protocol: 'http', host: 'localhost', port: 8000, path: '/happy/go/lucky', file: 'file.html' }
 
 function getPath(path){
   var sliced = [];
-  var startOfLastDash = path.lastIndexOf("/");
-  var startofFirst = path.indexOf("/")
-  var firstFolder = path.slice(0, startofFirst);
-  var secondFolder = path.slice(startofFirst+1, startOfLastDash);
-  var thirdFolder = path.slice(startOfLastDash+1, path.length);
-  sliced.push(firstFolder, secondFolder, thirdFolder);
-
+  sliced = path.split("/")
+  console.log(sliced)
   return sliced;
 }
 
-function getPathParts(){
-
+function getPathParts(url){
+  var objectReturn = {};
+  objectReturn.protocol = url.substring(0, url.indexOf(":"));
+  objectReturn.host = url.slice(url.indexOf("/") +2, url.lastIndexOf(":"));
+  objectReturn.port = url.slice(url.lastIndexOf(":") +1, url.lastIndexOf(":"));
+  objectReturn.path = url.slice(url.indexOf(":") +2, url.lastIndexOf("/"));
+  objectReturn.file = url.slice(url.lastIndexOf("/") + 1, url.length);
+  return objectReturn;
 }
 
 function getCapitalCount(){
