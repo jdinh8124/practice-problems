fa685fe862dcb6eb00c8261461e5cdd43901a8c7
James Dinh <mrjames8124@gmail.com>
2019-11-23 17:10:41 -0800

    finished drill03

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -1,21 +1,3 @@
-// - getPath: given a path string, return an array with the different parts of the path for
-// 	- example: 'folder1/folder2/folder3'
-//     - returns['folder1', 'folder2', 'folder3']
-
-// var findingTheStartOfSource = foundImg.lastIndexOf('/') + 1;
-// var findingSpotOfImgSource = foundImg.lastIndexOf('.');
-// var imageUrlWithoutPath = foundImg.slice(findingTheStartOfSource, findingSpotOfImgSource);
-// var startOfLastDash = path.lastIndexOf("/");
-// var startofFirst = path.indexOf("/")
-// var firstFolder = path.slice(0, startofFirst);
-// var secondFolder = path.slice(startofFirst + 1, startOfLastDash);
-// var thirdFolder = path.slice(startOfLastDash + 1, path.length);
-// sliced.push(firstFolder, secondFolder, thirdFolder);
-
-// - getPathParts: Given a full URL string, break it up into parts in an object For
-//   - example: 'http://localhost:8000/happy/go/lucky/file.html'
-//     - return { protocol: 'http', host: 'localhost', port: 8000, path: '/happy/go/lucky', file: 'file.html' }
-
 function getPath(path){
   var sliced = [];
   sliced = path.split("/")
@@ -33,14 +15,56 @@ function getPathParts(url){
   return objectReturn;
 }
 
-function getCapitalCount(){
-
+function getCapitalCount(array){
+  var capitalLetters = 0;
+  for(var i = 0; i < array.length; i++){
+    if (array[i][0] === array[i][0].toUpperCase()){
+      capitalLetters += 1;
+    }
+  }
+  return capitalLetters;
 }
 
-function correctCalcChecker(){
 
+// - correctCalcChecker: Given an array of objects with math equations, return all objects that have correct equations for
+// 	- example: [
+//     { num1: 3, num2: 3, op: '/', result: 3 },
+//     { num1: 12, num2: 4, op: '-', result: 8 },
+//     { num1: 2, num2: 3, op: '+', result: 5 },
+//     { num1: -5, num2: -2, op: '*', result: 10 }
+//   ]
+//     - returns[
+//     { num1: 12, num2: 4, op: '-', result: 8 },
+//     { num1: 2, num2: 3, op: '+', result: 5 }
+//     ]
+
+function correctCalcChecker(arrayMath){
+   var  newArrayToPush =[];
+  for(var i = 0; i < arrayMath.length; i++){
+    if(doMath(arrayMath[i].num1,arrayMath[i].op,arrayMath[i].num2) === arrayMath[i].result){
+        newArrayToPush.push(arrayMath[i]);
+    }
+  }
+  return newArrayToPush;
 }
 
-function doMath(){
-
+function doMath(num1, op, num2){
+  var mathResult = null;
+  switch(op){
+    case "+":
+      mathResult = num1 + num2;
+      break;
+    case "-":
+      mathResult = num1 - num2;
+      break;
+    case "*":
+    case "x":
+    case "X":
+      mathResult = num1 * num2;
+      break;
+    case "/":
+      mathResult = num1 / num2;
+      break;
+  }
+  return mathResult;
 }
